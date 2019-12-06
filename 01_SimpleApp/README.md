188ec23c39b00032cbf1ad8e64d92e3e93088420
Dennis Lee <dennisholee@gmail.com>
2019-12-06 22:16:33 +0800

    First draft

---
+++ 01_SimpleApp/README.md
@@ -0,0 +1,2 @@
+Build image `docker build -t 01_simpleapp .`
+Start container `docker run -d -p 3000:3000 01_simpleapp`
---
+++ 01_SimpleApp/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world')
+})
+
+app.listen(3000, () => {console.log('Listening on port 3000')})
