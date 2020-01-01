e9f2c7841ecf5319cb6def559ddca27622370ef4
iwasnothing <39053140+iwasnothing@users.noreply.github.com>
2020-01-01 22:28:40 +0800

    Update index.js

--- 01_SimpleApp/app/index.js
+++ 01_SimpleApp/app/index.js
@@ -60,7 +60,7 @@ api.get('/passengers/:phone', async(req, res) => {
 })
 
 api.post('/coords', function (req, res) {
-    writeCoords(req.body);
+    writeCoords(JSON.parse(req.body));
     console.log('write coords');
     res.status(200).send("successful");
 });
